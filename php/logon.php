b14a3c3d79042be83c2054d7de14414dac7ebdc2
Dandara Ramos <dandara.versutti@etec.sp.gov.br>
2024-10-07 21:42:36 -0300

    Tela de login com banco de dados

---
+++ php/logon.php
@@ -0,0 +1,27 @@
+<?php
+//recebendo os dados da tela de login
+
+$email = $_POST['email'];
+$password = $_POST['senha'];
+
+//link da conexão
+include 'conexao.php';
+
+//Pegar os dados do banco
+
+$sql ="SELECT * FROM tb_user WHERE email = '$email'";
+
+$query = $conexao->query($sql);
+
+$resultado = $query->fetch_assoc();
+
+$email_banco = $resultado['email'];
+$senha_banco = $resultado['senha'];
+
+if ($email == $email_banco && $password == $senha_banco) {
+    header('localtion: ../tela_inicial.html');
+}else {
+     echo "<script> alert('usuario ou senha invalida'); history.back(); </script>";
+}
+
+?>
